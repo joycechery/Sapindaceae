c93d1df5bc02241d03ae1f1381a2d6021fdb7b55
Joyce Gloria Chery <joycegchery@gmail.com>
2017-07-14 22:12:56 -0700

    Update AverageGeneCoverage.pl

--- FilterSequences/AverageGeneCoverage.pl
+++ FilterSequences/AverageGeneCoverage.pl
@@ -3,7 +3,7 @@
 use File::Slurp 'read_file';
 
 my $home = '/clusterfs/vector/scratch/cdspecht/Joyce_Chery/final/FINAL_ALIGNMENT/';
-my $genesFile = '/clusterfs/vector/scratch/cdspecht/Joyce_Chery/final/FINAL_ALIGNMENT/PlastidRemoved_Names';
+my $genesFile = '/clusterfs/vector/scratch/cdspecht/Joyce_Chery/final/FINAL_ALIGNMENT/LocalBlastRemoveNCBIRemove_Names';
 my $covFile = '/clusterfs/vector/scratch/cdspecht/Joyce_Chery/final/FINAL_ALIGNMENT/LocalBlastRemoveNCBIRemove_PositionCoverage';
 my @genes = read_file $genesFile;
 
